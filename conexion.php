9f36cd81b0eb705ec6c578f5c6416e9cd71d7f89
Karen Rodriguez <karenrodx@gmail.com>
2023-01-23 10:56:40 -0500

    Documentación funciones

--- conexion.php
+++ conexion.php
@@ -26,6 +26,11 @@ if(sizeof($info) > 0){
     print_r($res);
 }
 
+/**
+ * Función para traer la información especifica de cada pokemon
+ * @param mixed $id
+ * @return bool|string
+ */
 function getInfo($id){
     //echo $id;
 	$data_individual = file_get_contents('https://pokeapi.co/api/v2/pokemon/'.$id);
@@ -34,11 +39,20 @@ function getInfo($id){
 
 }
 
+/**
+ * Función para obtener todas las generaciones existentes en Pokemon
+ * @return bool|string
+ */
 function getAll(){
 $data = file_get_contents('https://pokeapi.co/api/v2/generation');
 	return $data;
 }
 
+/**
+ * Función para obtener la información especifica de cada generación
+ * @param mixed $id
+ * @return bool|string
+ */
 function getGenertion($id){
      return file_get_contents('https://pokeapi.co/api/v2/generation/'.$id);
     
